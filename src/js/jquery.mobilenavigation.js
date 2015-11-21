e4bf4e65bb033f3ebb7116d2966c4964bd457b48
Alexander Frühwirth <alexander.fruehwirth@gmail.com>
2015-11-21 13:11:10 +0100

    Add UMD support

--- src/js/jquery.mobilenavigation.js
+++ src/js/jquery.mobilenavigation.js
@@ -3,11 +3,42 @@
  *
  *  Plugin for a sliding mobilenavigation
  *
- *  author: alexfrueh
+ *  author: alexfruehwirth
+ *  repo: https://github.com/alexfruewhwirth/mobilenavigation
+ *  version: 0.2
  */
 
+
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+// see: https://github.com/umdjs/umd/blob/master/templates/jqueryPlugin.js
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}
 (function ($) {
-    //Todo: Add AMD (requireJs) support
     $.fn.mobilenavigation = function (options) {
 
         var constants = {
@@ -178,4 +209,4 @@
     };
 
 
-}(jQuery));
+}));
