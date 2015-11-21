b1e8bc808b79ac8f6662294a64902f54d7b7a3b8
Alexander Frühwirth <alexander.fruehwirth@gmail.com>
2015-11-21 22:26:44 +0100

    styling changes mobile

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
    var $nav = $('.menu').mobilenavigation({
       breakpoint: 768,
    });
+
+   $('.menu__toggle').click(function(){
+      $('.list--level-1').toggleClass('list--open');
+   });
 });
